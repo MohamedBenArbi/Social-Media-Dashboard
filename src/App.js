9995ebfa566b0ccf3b52495656d3f4577c4b25f5
MohamedBenArbi <88594502+MohamedBenArbi@users.noreply.github.com>
2022-07-12 00:06:55 +0100

    Created Component1 : Followers Tracker Component1 is Static for Now A Dynamic Component1 will be Added later

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Component1 from './Component1';
 
 function App() {
   return (
     <>
+      <Component1 />
     </>
   );
 }
---
+++ src/Component1.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import fb from './icon-facebook.svg'
+import up from './icon-up.svg'
+
+export default function Component1() {
+    return (
+
+        <div className='w-screen h-screen bg-Bl-300'>
+            <div className='flex flex-col items-center justify-around w-1/6 border-t-4 rounded-md border-Bl-200 bg-Bl-100 h-1/4 '>
+
+                <div className='flex items-center justify-center '>
+                    <img className='mr-2' src={fb} />
+                    <p className='text-sm text-gray-400'>@nathanf</p>
+                </div>
+
+                <div className='flex flex-col items-center justify-center'>
+                    <p className='text-5xl font-bold text-white'>1987</p>
+                    <p className='tracking-[4px] text-gray-600 '>FOLLOWERS</p>
+                </div>
+
+                <div className='flex items-center justify-center'>
+                    <img src={up} />
+                    <p className='font-bold text-Gr-100'> 12 Today</p>
+                </div>
+
+            </div>
+        </div>
+
+    )
+}

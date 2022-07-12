f125f86a0ba1662e858a39b4bea34add696c7c90
MohamedBenArbi <88594502+MohamedBenArbi@users.noreply.github.com>
2022-07-12 02:00:04 +0100

    Replaced static Component1 with dynamic Followers Section Completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
 import './App.css';
 import Component1 from './Component1';
+import fb from './icon-facebook.svg'
+import tw from './icon-twitter.svg'
+import ig from './icon-instagram.svg'
+import yt from './icon-youtube.svg'
+
+
+
 
 function App() {
   return (
     <>
-      <Component1 />
+      <div className='flex items-center justify-center w-screen h-screen bg-Bl-300'>
+
+        <div className='flex flex-col items-center w-screen h-screen p-8'>
+
+
+          <span className='text-3xl font-bold text-white'>Social Media Dashboard</span><br />
+          <span className='font-bold text-gray-400'>Total Followers : 23,004</span>
+
+
+          <div className='flex justify-center w-screen h-screen '>
+            <Component1 logo={fb} tp='fb' username="@nathanf" number="1987" today="12" td='+' type='FOLLOWERS' />
+            <Component1 logo={tw} tp='tw' username="@nathanf" number="1044" today="99" td='+' type='FOLLOWERS' />
+            <Component1 logo={ig} tp='ig' username="@realnathanf" number="11k" today="1099" td='+' type='FOLLOWERS' />
+            <Component1 logo={yt} tp='yt' username="@Nathan F." number="8239" today="144" td='-' type='SUBSCRIBERS' />
+
+          </div>
+
+
+
+        </div>
+
+
+
+
+
+
+      </div>
+
+
     </>
   );
 }
--- src/Component1.js
+++ src/Component1.js
@@ -1,30 +1,32 @@
 import React from 'react'
-import fb from './icon-facebook.svg'
 import up from './icon-up.svg'
+import down from './icon-down.svg'
 
-export default function Component1() {
+
+export default function Component1(props) {
     return (
 
-        <div className='w-screen h-screen bg-Bl-300'>
-            <div className='flex flex-col items-center justify-around w-1/6 border-t-4 rounded-md border-Bl-200 bg-Bl-100 h-1/4 '>
 
-                <div className='flex items-center justify-center '>
-                    <img className='mr-2' src={fb} />
-                    <p className='text-sm text-gray-400'>@nathanf</p>
-                </div>
+        <div className={'flex flex-col items-center justify-around w-1/6 m-3 border-t-4 rounded-md bg-Bl-100 h-1/4 ' + (props.tp == 'fb' ? 'border-Bl-200' : '') + (props.tp == 'ig' ? 'border-purple-400' : '') + (props.tp == 'tw' ? 'border-Bl-400' : '') + (props.tp == 'yt' ? 'border-Rd-100' : '')}
+        >
 
-                <div className='flex flex-col items-center justify-center'>
-                    <p className='text-5xl font-bold text-white'>1987</p>
-                    <p className='tracking-[4px] text-gray-600 '>FOLLOWERS</p>
-                </div>
-
-                <div className='flex items-center justify-center'>
-                    <img src={up} />
-                    <p className='font-bold text-Gr-100'> 12 Today</p>
-                </div>
-
+            <div className='flex items-center justify-center '>
+                <img className='mr-2' src={props.logo} />
+                <p className='text-sm text-gray-400'>{props.username}</p>
             </div>
+
+            <div className='flex flex-col items-center justify-center'>
+                <p className='text-5xl font-bold text-white'>{props.number}</p>
+                <p className='tracking-[4px] text-gray-600 '>{props.type}</p>
+            </div>
+
+            <div className='flex items-center justify-center'>
+                <img src={`${props.td === '+' ? up : down}`} />
+                <p className={` ${props.td === '+' ? 'text-Gr-100 font-bold' : 'text-red-800 font-bold'} `}> {props.today} Today</p>
+            </div>
+
         </div>
+
 
     )
 }
